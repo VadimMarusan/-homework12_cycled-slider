b7a09cc9e3a8a7976fbfb51d833314c380874ab8
Vadim <wazzupvadim@gmail.com>
2020-10-01 22:56:27 +0300

    add hm12

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let slides = document.querySelectorAll('.slide-to-show');
+let currentSlide = 0;
+let slideInterval = setInterval(nextSlide,3000);
+let pause = document.getElementById("pause");
+let play = document.getElementById("play");
+
+function nextSlide(){
+    slides[currentSlide].className = 'slide-to-show';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide-to-show showing';
+}
+
+
+
+pause.onclick = function pauseSlider () {
+    clearInterval(slideInterval);
+}
+
+play.onclick = function playSlider() {
+    slideInterval = setInterval(nextSlide,3000);
+}
+
+/*
+function timer (){
+    let timeLeft = document.querySelector(".display");
+    let currentTime = parseInt(timeLeft.innerHTML);
+    let time = setInterval(function () {
+            if (--currentTime >= 0) { // если таймер всё еще больше нуля
+                timeLeft.innerHTML = currentTime // обновляем цифру
+            }
+        }, 1000)
+
+}
+timer(); */
+
+
+
+
